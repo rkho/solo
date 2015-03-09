8a9e016ad90facbffa08c121e56969867c2721d2
Richard Kho <rkho@me.com>
2015-03-09 15:26:02 -0700

    Can retreive objects from Firebase

--- app/scripts/controllers/firelink.js
+++ app/scripts/controllers/firelink.js
@@ -19,14 +19,25 @@
 
 
 angular.module('lunchrecsApp', ['firebase'])
-  .controller('FirelinkCtrl', function($scope, $firebaseArray){
+  .controller('FirelinkCtrl', function($scope, $firebaseArray, $http){
     var ref = new Firebase('https://lunchrecs.firebaseio.com/places');
+    var placesArr = [];
     $scope.places = $firebaseArray(ref);
     $scope.addPlace = function(){
-      console.log('hello');
       $scope.places.$add({
         text: 'Chipotle'
       });
     }
+    $scope.getPlace = function(){
+      // $http({method: 'JSON', url: 'https://lunchrecs.firebaseio.com/places'})
+      //   .success(function(data, status){
+      //     console.log(data);
+      //   })
+      jQuery.getJSON('https://lunchrecs.firebaseio.com/places.json', function(data){
+        _.each(data, function(elem){
+          console.log(elem.text);
+        })
+      });
+      };
   });
 //
