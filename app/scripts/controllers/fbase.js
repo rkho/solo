81890c19f282bc5f73eeeef15bea99fe5f67f7f1
Richard Kho <rkho@me.com>
2015-03-09 19:49:11 -0700

    Modal now pops up when you press on 'Lets Go' button

--- app/scripts/controllers/fbase.js
+++ app/scripts/controllers/fbase.js
@@ -1,5 +1,5 @@
 angular.module('lunchRecs')
-  .controller('FirebaseCtrl', function($scope, $firebaseArray, $http){
+  .controller('FirebaseCtrl', function($scope, $firebaseArray){
     var ref = new Firebase('https://lunchrecs.firebaseio.com/places');
     var placesArr = [];
     $scope.places = $firebaseArray(ref);
@@ -9,14 +9,44 @@ angular.module('lunchRecs')
       });
     }
     $scope.getPlace = function(){
-      // $http({method: 'JSON', url: 'https://lunchrecs.firebaseio.com/places'})
-      //   .success(function(data, status){
-      //     console.log(data);
-      //   })
       jQuery.getJSON('https://lunchrecs.firebaseio.com/places.json', function(data){
         _.each(data, function(elem){
           console.log(elem.text);
         })
       });
-      };
-  });
+    };
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+        console.log('hi!')
+        $scope.showModal = !$scope.showModal;
+    };
+  })
+  .directive('modal', function(){
+    return {
+      templateUrl: 'views/modal.html',
+      restrict: 'E',
+      replace: true,
+      link: function (scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+      }
+    });
