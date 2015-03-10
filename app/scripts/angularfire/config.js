fdf035ad9e0b671ff914605d796a9e53d7f90e98
Richard Kho <rkho@me.com>
2015-03-10 15:13:13 -0700

    Removing deleted files

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://lunchrecs.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','github'])
-
-  .constant('loginRedirectPath', '/login');
